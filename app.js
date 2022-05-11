85fabacc6fb6a7fd4c647c32c0b3b5b7caee6e76
Razalkabeer <razalkabeer@hotmail.com>
2022-05-11 14:23:44 +0530

    Changed /work route post method

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 let day = "";
 let todoInputs = [];
+let workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -20,14 +21,30 @@ app.get("/", (req, res) => {
 
     let day = today.toLocaleString("en-US", options);
 
-    res.render("list", { kindOfDay: day, newTodoInputs: todoInputs });
+    res.render("list", { listTitle: day, newTodoInputs: todoInputs });
 });
 
 app.post("/", (req, res) => {
-    var todoInput = req.body.todoInput;
-    todoInputs.push(todoInput);
-    res.redirect("/");
+    let todoInput = req.body.todoInput;
+
+    if (req.body.list === "Work") {
+        workItems.push(todoInput);
+        res.redirect("/work");
+    } else {
+        todoInputs.push(todoInput);
+        res.redirect("/");
+    }
 });
+
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work List", newTodoInputs: workItems });
+});
+
+// app.post("/work", (req, res) => {
+//     let item = req.body.todoInput;
+//     workItems.push(item);
+//     res.redirect("/work");
+// });
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
